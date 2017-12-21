40dea347b1927c0e1c8e14af4cb4bd24912e7001
behrad nasehi <behrad.nasehi@chmail.ir>
2017-12-21 14:39:54 +0330

    Add files via upload

---
+++ utility.js
@@ -0,0 +1,22 @@
+﻿/* isNumberKey
+   Only allows NUMBERS to be keyed into a text field.
+   @environment ALL
+   @param evt - The specified EVENT that happens on the element.
+   @return True if number, false otherwise.
+*/
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    
+    /*
+    Key    |Code 
+    ------------
+    delete | 46
+    0 	   | 48     
+    9 	   | 57
+     */
+
+    if (charCode > 46 && (charCode < 48 || charCode > 57))
+        return false;
+
+    return true;
+} // isNumberKey
